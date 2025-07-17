acd794bfac1f012a49e93018579dfe5f340a5859
Parthiv070 <parthivchudasama5@gmail.com>
2025-07-17 10:03:50 +0530

    add sequence generator.

---
+++ sweet-shop/src/main/java/com/sweetshop/service/SequenceGeneratorService.java
@@ -0,0 +1,27 @@
+package com.sweetshop.service;
+
+import com.sweetshop.model.Counter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SequenceGeneratorService {
+
+    @Autowired
+    private MongoOperations mongoOperations;
+
+    public long getNextSequence(String seqName) {
+        Counter counter = mongoOperations.findAndModify(
+                Query.query(Criteria.where("_id").is(seqName)),
+                new Update().inc("seq", 1),
+                FindAndModifyOptions.options().returnNew(true).upsert(true),
+                Counter.class);
+
+        return (counter != null) ? counter.getSeq() + 1000 : 1001;
+    }
+}
